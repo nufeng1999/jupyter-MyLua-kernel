5ebdcfe3698cbeaa5b54e766ae0a4eef3f5383ec
nufeng1999 <18478162@21cn.com>
2022-01-07 22:52:33 +0800

    unforced import zerorpc

--- jupyter_MyLua_kernel/MyMagics.py
+++ jupyter_MyLua_kernel/MyMagics.py
@@ -11,7 +11,6 @@ from shutil import copyfile,move
 from urllib.request import urlopen
 import socket
 import copy
-import zerorpc
 import mmap
 import contextlib
 import atexit
@@ -41,6 +40,11 @@ import inspect
 from . import ipynbfile
 from plugins.ISpecialID import IStag,IDtag,IBtag,ITag,ICodePreproc
 from plugins._filter2_magics import Magics
+try:
+    zerorpc=__import__("zerorpc")
+    # import zerorpc
+except:
+    pass
 fcntl = None
 msvcrt = None
 bLinux = True
